c3cbca6c524a5593f7aef34567a30d36794d474e
298A-E9E3 <166412009+298A-E9E3@users.noreply.github.com>
2025-12-03 16:32:53 -0500

    Update SensorMotor.java

--- src/main/java/frc/robot/Subsystems/SensorMotor.java
+++ src/main/java/frc/robot/Subsystems/SensorMotor.java
@@ -5,10 +5,13 @@
 package frc.robot.Subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+// import frc.robot.Constants;
 
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel;
+
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
@@ -16,16 +19,23 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 public class PhotoMotor extends SubsystemBase {
   /** Create a motor */
+  SparkMax motor = new SparkMax(0,SparkLowLevel.MotorType.kBrushless);
   /** Use photo electric sensor 1 */
-  }
-    public boolean getPhotoElectricSensor() {
+  AnalogInput photoSensor = new AnalogInput(0);
+  public boolean getPhotoElectricSensor() {
     /** detects if there is an object */
+    
+    return photoSensor.getVoltage() < 2;
   }
   public PhotoMotor() {}
 
   @Override
   public void periodic() {
     /** make it spin at 25% if an object is detected */
-
+    if(getPhotoElectricSensor()){
+      motor.set(0.25);
+    }else{
+      motor.set(0);
+    }
   }
 }
